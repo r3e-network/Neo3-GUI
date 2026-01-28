32c50ed40b6e0de9ec1aa5ca4396b44e2da99036
Jimmy <jimmy@r3e.network>
2026-01-28 23:39:40 +0800

    fix: Round 4 - LevelDbContext 资源管理优化
    1. 移除未使用的 using 语句
    2. 使用 GetOrAdd 替代 ContainsKey+索引器 (原子操作)
    3. WriteBatch 正确实现 Dispose 模式
    4. 移除未使用的 _dbPath 字段
    5. 前缀数组改为 static readonly (性能优化)

--- neo3-gui/neo3-gui/Common/Storage/LevelDBModules/LevelDbContext.cs
+++ neo3-gui/neo3-gui/Common/Storage/LevelDBModules/LevelDbContext.cs
@@ -1,41 +1,35 @@
-﻿using System;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Numerics;
-using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading.Tasks;
 using Neo.Common.Consoles;
 using Neo.Extensions;
-using Neo.IO;
 using Neo.Models;
 using Neo.VM.Types;
 
 namespace Neo.Common.Storage.LevelDBModules
 {
+    /// <summary>
+    /// LevelDB context for blockchain data storage
+    /// </summary>
     public class LevelDbContext : IDisposable
     {
-        private static ConcurrentDictionary<string, DB> _cache = new ConcurrentDictionary<string, DB>();
+        private static readonly ConcurrentDictionary<string, DB> _cache = new();
 
-        private DB _db;
-        private string _dbPath;
+        private readonly DB _db;
+        private WriteBatch _writeBatch = new();
+        private bool _disposed;
 
-        private readonly byte[] ExecuteLogPrefix = { 0xff };
-        //private readonly byte[] SyncIndexPrefix = { 0xfe };
-        private readonly byte[] MaxSyncIndexPrefix = { 0xfd };
-        private readonly byte[] AssetPrefix = { 0xfc };
-        private readonly byte[] BalancePrefix = { 0xfb };
-        private readonly byte[] TransferPrefix = { 0xfa };
-        private readonly byte[] ContractEventPrefix = { 0xf9 };
-        private readonly byte[] BalanceChangingPrefix = { 0xf8 };
-
-
-
-
-
-        private WriteBatch writeBatch = new WriteBatch();
+        private static readonly byte[] ExecuteLogPrefix = { 0xff };
+        private static readonly byte[] MaxSyncIndexPrefix = { 0xfd };
+        private static readonly byte[] AssetPrefix = { 0xfc };
+        private static readonly byte[] BalancePrefix = { 0xfb };
+        private static readonly byte[] TransferPrefix = { 0xfa };
+        private static readonly byte[] ContractEventPrefix = { 0xf9 };
+        private static readonly byte[] BalanceChangingPrefix = { 0xf8 };
 
         static LevelDbContext()
         {
@@ -44,23 +38,16 @@ namespace Neo.Common.Storage.LevelDBModules
                 Directory.CreateDirectory("Data_Track");
             }
         }
-        public LevelDbContext() : this(Path.Combine("Data_Track", $"TransactionLog_LevelDB_{CliSettings.Default.Protocol.Network}"))
+
+        public LevelDbContext() 
+            : this(Path.Combine("Data_Track", $"TransactionLog_LevelDB_{CliSettings.Default.Protocol.Network}"))
         {
         }
 
-
         public LevelDbContext(string dbPath)
         {
-            _dbPath = dbPath;
-            if (_cache.ContainsKey(dbPath))
-            {
-                _db = _cache[dbPath];
-            }
-            else
-            {
-                _db = DB.Open(dbPath, new Options { CreateIfMissing = true });
-                _cache[dbPath] = _db;
-            }
+            _db = _cache.GetOrAdd(dbPath, path => 
+                DB.Open(path, new Options { CreateIfMissing = true }));
         }
 
         private byte[] ContractEventKey(uint height) => ContractEventPrefix.Append(BitConverter.GetBytes(height));
@@ -78,10 +65,8 @@ namespace Neo.Common.Storage.LevelDBModules
         }
 
         /// <summary>
-        /// save current block contract change(create,destroy,migrate) info
+        /// Save current block contract change (create, destroy, migrate) info
         /// </summary>
-        /// <param name="height"></param>
-        /// <param name="value"></param>
         public void SaveContractEvent(uint height, IDictionary<UInt256, List<ContractEventInfo>> value)
         {
             if (value.NotEmpty())
@@ -92,38 +77,31 @@ namespace Neo.Common.Storage.LevelDBModules
                     TxId = kv.Key,
                     Events = kv.Value,
                 });
-                writeBatch.Put(key, storageValue.SerializeJsonBytes());
+                _writeBatch.Put(key, storageValue.SerializeJsonBytes());
             }
         }
-
 
         private byte[] ExecuteLogKey(UInt256 txId) => ExecuteLogPrefix.Append(txId.ToArray());
 
         /// <summary>
-        /// save execute result log after call <see cref="Commit"/> method
+        /// Save execute result log after call Commit method
         /// </summary>
-        /// <param name="log"></param>
         public void SaveTxExecuteLog(ExecuteResultInfo log)
         {
-            if (log != null && log.TxId != null)
+            if (log?.TxId != null)
             {
                 try
                 {
-                    writeBatch.Put(ExecuteLogKey(log.TxId), log.SerializeJsonBytes());
+                    _writeBatch.Put(ExecuteLogKey(log.TxId), log.SerializeJsonBytes());
                 }
                 catch (Exception e)
                 {
                     log.ResultStack = new StackItem[] { e.ToString() };
-                    writeBatch.Put(ExecuteLogKey(log.TxId), log.SerializeJsonBytes());
+                    _writeBatch.Put(ExecuteLogKey(log.TxId), log.SerializeJsonBytes());
                 }
             }
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="txId"></param>
-        /// <returns></returns>
         public ExecuteResultInfo GetExecuteLog(UInt256 txId)
         {
             var value = _db.Get(ReadOptions.Default, ExecuteLogKey(txId));
@@ -142,10 +120,9 @@ namespace Neo.Common.Storage.LevelDBModules
             var value = _db.Get(key);
             if (value == null)
             {
-                writeBatch.Put(key, assetInfo.SerializeJsonBytes());
+                _writeBatch.Put(key, assetInfo.SerializeJsonBytes());
             }
         }
-
 
         public AssetInfo GetAssetInfo(UInt160 assetId)
         {
@@ -156,96 +133,68 @@ namespace Neo.Common.Storage.LevelDBModules
 
         private byte[] BalanceAccountsKey(uint height) => BalanceChangingPrefix.Append(BitConverter.GetBytes(height));
 
-
         /// <summary>
         /// Save Balance-Changed Accounts in Specified Block
         /// </summary>
-        /// <param name="height"></param>
-        /// <param name="accounts"></param>
         public void UpdateBalancingAccounts(uint height, HashSet<AccountAsset> accounts)
         {
             var key = BalanceAccountsKey(height);
-            writeBatch.Put(key, accounts.SerializeJsonBytes());
+            _writeBatch.Put(key, accounts.SerializeJsonBytes());
         }
 
         /// <summary>
         /// Get Balance-Changed Accounts in Specified Block
         /// </summary>
-        /// <param name="height"></param>
-        /// <returns></returns>
         public HashSet<AccountAsset> GetBalancingAccounts(uint height)
         {
             var key = BalanceAccountsKey(height);
-            var value = _db.Get(key).DeserializeJson<HashSet<AccountAsset>>();
-            return value;
+            return _db.Get(key).DeserializeJson<HashSet<AccountAsset>>();
         }
 
-
         private byte[] BalanceKey(UInt160 account, UInt160 asset) => BalancePrefix.Append(account.ToArray(), asset.ToArray());
+
         public void UpdateBalance(UInt160 account, UInt160 asset, BigInteger balance, uint height)
         {
             var key = BalanceKey(account, asset);
-            var balanceRecord = new BalanceStorageItem() { Balance = balance, Height = height };
-            writeBatch.Put(key, balanceRecord.SerializeJsonBytes());
+            var balanceRecord = new BalanceStorageItem { Balance = balance, Height = height };
+            _writeBatch.Put(key, balanceRecord.SerializeJsonBytes());
         }
-
 
         public BalanceStorageItem GetBalance(UInt160 account, UInt160 asset)
         {
             var key = BalanceKey(account, asset);
-            var value = GetBalance(key);
-            return value;
+            return GetBalance(key);
         }
 
         private BalanceStorageItem GetBalance(byte[] balanceKey)
         {
             var value = _db.Get(balanceKey);
-            if (value.NotEmpty())
-            {
-                return value.DeserializeJson<BalanceStorageItem>();
-            }
-            return null;
+            return value.NotEmpty() ? value.DeserializeJson<BalanceStorageItem>() : null;
         }
-
 
         private byte[] TransferKey(uint blockHeight) => TransferPrefix.Append(BitConverter.GetBytes(blockHeight));
 
-
         /// <summary>
-        /// will save after call <see cref="Commit"/> method
+        /// Save transfers after call Commit method
         /// </summary>
-        /// <param name="blockHeight"></param>
-        /// <param name="transfers"></param>
         public void SaveTransfers(uint blockHeight, List<TransferStorageItem> transfers)
         {
             var key = TransferKey(blockHeight);
-            writeBatch.Put(key, transfers.SerializeJsonBytes());
+            _writeBatch.Put(key, transfers.SerializeJsonBytes());
         }
-
 
         public List<TransferStorageItem> GetTransfers(uint blockHeight)
         {
             var key = TransferKey(blockHeight);
             var value = _db.Get(key);
-            if (value.NotEmpty())
-            {
-                return value.DeserializeJson<List<TransferStorageItem>>();
-            }
-            return null;
+            return value.NotEmpty() ? value.DeserializeJson<List<TransferStorageItem>>() : null;
         }
 
-
-        //private byte[] SyncIndexKey(byte[] db, uint height) => SyncIndexPrefix.Append(db, BitConverter.GetBytes(height));
         private byte[] MaxSyncIndexKey(byte[] db) => MaxSyncIndexPrefix.Append(db);
 
-
-
         /// <summary>
-        /// is this height synced? true:yes
+        /// Check if height is synced
         /// </summary>
-        /// <param name="dbPrefix"></param>
-        /// <param name="height"></param>
-        /// <returns></returns>
         public bool HasSyncIndex(byte[] dbPrefix, uint height)
         {
             var max = GetMaxSyncIndex(dbPrefix);
@@ -253,56 +202,59 @@ namespace Neo.Common.Storage.LevelDBModules
         }
 
         /// <summary>
-        /// save synced max height after call <see cref="Commit"/> method
+        /// Save synced max height after call Commit method
         /// </summary>
-        /// <param name="dbPrefix"></param>
-        /// <param name="height"></param>
         public void SetMaxSyncIndex(byte[] dbPrefix, uint height)
         {
             var max = GetMaxSyncIndex(dbPrefix);
             if (max == null || max < height)
             {
                 SetMaxSyncIndexForce(dbPrefix, height);
-                //_db.Put(WriteOptions.Default, MaxSyncIndexKey(dbPrefix), BitConverter.GetBytes(height));
             }
         }
 
         /// <summary>
-        /// save synced max height after call <see cref="Commit"/> method
+        /// Force save synced max height after call Commit method
         /// </summary>
-        /// <param name="dbPrefix"></param>
-        /// <param name="height"></param>
         public void SetMaxSyncIndexForce(byte[] dbPrefix, uint height)
         {
-            writeBatch.Put(MaxSyncIndexKey(dbPrefix), BitConverter.GetBytes(height));
+            _writeBatch.Put(MaxSyncIndexKey(dbPrefix), BitConverter.GetBytes(height));
         }
 
         public uint? GetMaxSyncIndex(byte[] dbPrefix)
         {
             var max = _db.Get(ReadOptions.Default, MaxSyncIndexKey(dbPrefix));
-            if (max == null)
-            {
-                return null;
-            }
-            return BitConverter.ToUInt32(max);
+            return max == null ? null : BitConverter.ToUInt32(max);
         }
 
-        
         public void Commit()
         {
-            _db.Write(WriteOptions.Default, writeBatch);
+            _db.Write(WriteOptions.Default, _writeBatch);
             ResetWriteBatch();
         }
 
         private void ResetWriteBatch()
         {
-            writeBatch?.Clear();
-            writeBatch = new WriteBatch();
+            _writeBatch?.Dispose();
+            _writeBatch = new WriteBatch();
         }
 
         public void Dispose()
         {
-            writeBatch?.Clear();
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _writeBatch?.Dispose();
+                }
+                _disposed = true;
+            }
         }
     }
 }
