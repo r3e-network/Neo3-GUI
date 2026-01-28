f2d1cc03114f7cc79f171d73d383c75572d2ab2a
Jimmy <jimmy@r3e.network>
2026-01-28 19:45:24 +0800

    test: stabilize frontend jest

--- neo3-gui/neo3-gui/ClientApp/src/setupTests.js
+++ neo3-gui/neo3-gui/ClientApp/src/setupTests.js
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+if (!window.matchMedia) {
+  window.matchMedia = () => ({
+    matches: false,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  });
+}
