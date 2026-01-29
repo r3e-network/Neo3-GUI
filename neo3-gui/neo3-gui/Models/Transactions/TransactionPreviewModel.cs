fc23bdfafe53e0bdf9d3e6a237d2396cb2bebd4d
Jimmy <jimmy@r3e.network>
2026-01-29 22:16:17 +0800

    refactor(TransactionPreviewModel): remove unused using statements
    Remove unused imports: Linq, Text, Threading.Tasks, Network.P2P.Payloads

--- neo3-gui/neo3-gui/Models/Transactions/TransactionPreviewModel.cs
+++ neo3-gui/neo3-gui/Models/Transactions/TransactionPreviewModel.cs
@@ -1,9 +1,5 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Neo.Network.P2P.Payloads;
 
 namespace Neo.Models.Transactions
 {
