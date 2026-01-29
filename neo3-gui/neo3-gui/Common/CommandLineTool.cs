6968df0426d17ae71479298526174b4e9c4fcf3d
Jimmy <jimmy@r3e.network>
2026-01-29 22:08:58 +0800

    refactor(CommandLineTool): clean up code and use expression-bodied members
    - Use expression-bodied members for shell and shellArg properties
    - Remove stray semicolon and Chinese comments
    - Remove unused commented code

--- neo3-gui/neo3-gui/Common/CommandLineTool.cs
+++ neo3-gui/neo3-gui/Common/CommandLineTool.cs
@@ -19,21 +19,9 @@ namespace Neo.Common
 
         public static readonly ConcurrentQueue<Process> CurrentProcesses = new ConcurrentQueue<Process>();
 
-        private static string shell
-        {
-            get
-            {
-                return IsWindows ? WindowsShell : UnixShell;
-            }
-        }
+        private static string shell => IsWindows ? WindowsShell : UnixShell;
 
-        private static string shellArg
-        {
-            get
-            {
-                return IsWindows ? WindowsShellArg : UnixShellArg;
-            }
-        }
+        private static string shellArg => IsWindows ? WindowsShellArg : UnixShellArg;
         
 
         public static Process Run(string command, string workDirectory = "")
@@ -60,21 +48,12 @@ namespace Neo.Common
         public static Process Run(string command, string workDirectory = "", Action<string> receiveOutput = null)
         {
             Process p = new Process();
-            //设置要启动的应用程序
             p.StartInfo.FileName = shell;
             p.StartInfo.WorkingDirectory = workDirectory;
-            //是否使用操作系统shell启动
             p.StartInfo.UseShellExecute = false;
-            // 接受来自调用程序的输入信息
             p.StartInfo.RedirectStandardInput = true;
-            //输出信息
             p.StartInfo.RedirectStandardOutput = true;
-            // 输出错误
             p.StartInfo.RedirectStandardError = true;
-            //p.StartInfo.StandardOutputEncoding=Encoding.Unicode;
-            ;
-            //不显示程序窗口
-            //p.StartInfo.CreateNoWindow = true;
             p.OutputDataReceived += (s, r) =>
             {
                 if (r.Data == null)
@@ -83,10 +62,7 @@ namespace Neo.Common
                     p = null;
                     return;
                 }
-                //Console.WriteLine(r.Data);
-                //receiveOutput?.Invoke(r.Data);
             };
-            //启动程序
             p.Start();
             p.StandardInput.WriteLine(command);
             p.BeginOutputReadLine();
