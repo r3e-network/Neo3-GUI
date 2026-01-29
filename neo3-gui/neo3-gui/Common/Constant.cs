1f595b23477c59ea9b32aded26a5a2ff8200c309
Jimmy <jimmy@r3e.network>
2026-01-29 19:22:44 +0800

    refactor(Constant): add documentation and common pagination constants

--- neo3-gui/neo3-gui/Common/Constant.cs
+++ neo3-gui/neo3-gui/Common/Constant.cs
@@ -8,6 +8,19 @@ namespace Neo.Common
 {
     public static class Constant
     {
+        /// <summary>
+        /// Maximum GAS for test mode execution (2000 GAS)
+        /// </summary>
         public const long TestMode = 2000_00000000;
+
+        /// <summary>
+        /// Default page size for pagination
+        /// </summary>
+        public const int DefaultPageSize = 100;
+
+        /// <summary>
+        /// Default page index (1-based)
+        /// </summary>
+        public const int DefaultPageIndex = 1;
     }
 }
