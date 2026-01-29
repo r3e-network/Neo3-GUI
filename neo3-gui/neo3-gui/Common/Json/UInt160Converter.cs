6156f8aa1e98497bd3e09aef7309485441d99bd9
Jimmy <jimmy@r3e.network>
2026-01-29 22:18:20 +0800

    refactor(UInt160Converter): remove unused using statements
    Remove unused imports: Collections.Generic, Linq, Runtime, Text, Threading.Tasks, Wallets

--- neo3-gui/neo3-gui/Common/Json/UInt160Converter.cs
+++ neo3-gui/neo3-gui/Common/Json/UInt160Converter.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime;
-using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-using Neo.Wallets;
 
 namespace Neo.Common.Json
 {
