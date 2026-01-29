c99e41eab30376b461a19c96d9b75c5ec62d7172
Jimmy <jimmy@r3e.network>
2026-01-29 22:11:33 +0800

    refactor(SyncHeightJob): remove unused using statements
    Remove unused imports: Collections.Generic, Linq, Text, Ledger,
    Network.P2P, SmartContract.Native

--- neo3-gui/neo3-gui/Models/Jobs/SyncHeightJob.cs
+++ neo3-gui/neo3-gui/Models/Jobs/SyncHeightJob.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Neo.Common.Scanners;
-using Neo.Ledger;
-using Neo.Network.P2P;
-using Neo.SmartContract.Native;
 
 namespace Neo.Models.Jobs
 {
